a74528d4e6fd212f341e50a73f40393abacc8923
CodeFormula1 <144355936+CodeFormula1@users.noreply.github.com>
2023-11-18 15:25:11 -0500

    Update model.py

--- model.py
+++ model.py
@@ -131,7 +131,7 @@
 
 
 import streamlit as st
-import seaborn as sns
+# import seaborn as sns
 # import matplotlib.pyplot as plt
 # import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
 import os
