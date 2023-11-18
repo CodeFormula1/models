71aa5cb59e36b3aed645fc0291fe405b7c288d2e
CodeFormula1 <144355936+CodeFormula1@users.noreply.github.com>
2023-11-18 15:31:17 -0500

    Update model.py

--- model.py
+++ model.py
@@ -196,7 +196,7 @@ elif page == "Data Analysis":
     if csv_file is not None:
             st.write("File uploaded successfully!")
             import streamlit as st
-            import seaborn as sns
+            # import seaborn as sns
             # import matplotlib.pyplot as plt
             # import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
             # import os
