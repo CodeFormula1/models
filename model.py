3dcd884ae3bb8309a2b04f67711a2c44690af668
CodeFormula1 <144355936+CodeFormula1@users.noreply.github.com>
2023-11-18 15:26:36 -0500

    Update model.py

--- model.py
+++ model.py
@@ -197,8 +197,8 @@ elif page == "Data Analysis":
             st.write("File uploaded successfully!")
             import streamlit as st
             # import seaborn as sns
-            import matplotlib.pyplot as plt
-            import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
+            # import matplotlib.pyplot as plt
+            # import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
             # import os
             import pandas as pd
 
