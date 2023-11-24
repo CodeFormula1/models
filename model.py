65db295e458c22320a7d1a146e31b49dd6c017ff
CodeFormula1 <144355936+CodeFormula1@users.noreply.github.com>
2023-11-24 13:45:03 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,141 +1,9 @@
-# """THIS CODE WORKS WELL FOR SINGLE PAGE """
-# import streamlit as st
-# import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
-
-# st.set_page_config(
-#     page_title="Vector Sport Data Analysis",
-#     page_icon="📈",
-# )
-
-
-# st.sidebar.success("Select a demo above.")
-
-# # Define the function to convert LAP_TIME to seconds
-# def lap_time_to_seconds(lap_time_str):
-#     minutes, seconds = map(float, lap_time_str.split(':'))
-#     return minutes * 60 + seconds
-
-# # Streamlit app title
-# st.title("Vector Sport Laps Analysis")
-
-# # Create a file uploader widget for the 'laps' DataFrame
-# csv_file = st.file_uploader("Upload Laps CSV File", type=["csv"])
-
-# @st.cache_data
-# def load_laps_data(nrows):
-#     data = pd.read_csv(csv_file, nrows=nrows)
-#     # If you have a 'DATE_COLUMN', you can convert it to datetime here
-#     # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-# if csv_file is not None:
-#     st.write("File uploaded successfully!")
-
-    
-#     laps = load_laps_data(10000)
-
-#     if st.checkbox('Show raw data'):
-#         st.subheader('Raw Laps data')
-#         st.write(laps.head())
-
-#         # Apply the function to the LAP_TIME column
-#         laps['LAP_TIME'] = laps['LAP_TIME'].apply(lap_time_to_seconds)
-
-#         # Use st.slider for LAP_TIME filter (same filter as the first graph)
-#         lap_time_filter = st.slider("Filter by LAP_TIME", min_value=float(laps['LAP_TIME'].min()), max_value=float(laps['LAP_TIME'].max()), value=float(laps['LAP_TIME'].max() - 1))
-#         filtered_laps = laps[laps['LAP_TIME'] < lap_time_filter]
-
-#         # Filter the data for 'Vector Sport' team
-#         vector_sport_laps = filtered_laps[filtered_laps['TEAM'] == 'Vector Sport']
-
-#         # Define a dictionary to map the Team names to hex codes
-#         team_colors = {'Vector Sport': '#008080'}
-
-#         # Group the data by 'TEAM'
-#         grouped = vector_sport_laps.groupby('TEAM')
-
-#         # Convert the grouped data to a DataFrame
-#         grouped_df = pd.DataFrame(grouped.apply(lambda x: x.reset_index(drop=True)))
-
-#         # Use lmplot with the new grouped DataFrame
-#         g = sns.lmplot(x='LAP_NUMBER', y='LAP_TIME', hue='TEAM', data=grouped_df, markers='o', palette=team_colors, scatter_kws={'s': 50})
-
-#         # Set the inset background to black
-#         g.ax.set_facecolor('black')
-
-#         # Add a title
-#         st.subheader('Fuel Corrected vs. Tyre Life by Team')
-#         st.pyplot(plt)
-
-#         # Add a section header for the second graph
-#         st.subheader("Second Graph")
-
-#         # Filter the data for 'Vector Sport' team (similar to the first plot)
-#         vector_sport_laps_second = laps[(laps['TEAM'] == 'Vector Sport') & (laps['LAP_TIME'] < lap_time_filter)]
-
-#         # Group the data by "Team" (for the second plot)
-#         grouped_second = vector_sport_laps_second.groupby('TEAM')
-
-#         # Convert the grouped data to a DataFrame
-#         grouped_df_second = pd.DataFrame(grouped_second.apply(lambda x: x.reset_index(drop=True)))
-
-#         # Initialize an empty list to store the slopes and team names
-#         slopes = []
-#         teams = []
-
-#         # Use scatterplot with the new grouped DataFrame (for the second plot)
-#         plt.figure(facecolor='black')
-#         sns.scatterplot(x='LAP_NUMBER', y='LAP_TIME', hue='TEAM', data=grouped_df_second, markers='o', s=50)
-
-#         # Calculate the slope using the lowess regression line
-#         for level in vector_sport_laps_second['TEAM'].unique():
-#             x = vector_sport_laps_second[vector_sport_laps_second['TEAM'] == level]['LAP_NUMBER']
-#             y = vector_sport_laps_second[vector_sport_laps_second['TEAM'] == level]['LAP_TIME']
-#             lowess = sm.nonparametric.lowess(y, x, frac=0.3)  # adjust the 'frac' parameter to control the smoothing
-#             slope = (lowess[-1, 1] - lowess[0, 1]) / (lowess[-1, 0] - lowess[0, 0])
-#             slopes.append(slope)
-#             teams.append(level)
-#             plt.plot(lowess[:, 0], lowess[:, 1], linewidth=2)
-
-#             # Print the team name and slope
-#             st.write('TEAM:', level, 'Slope:', slope)
-
-#         # Customize legend text color and remove legend outline
-#         legend = plt.legend(title='Team', loc='upper left', frameon=False)
-#         for text in legend.get_texts():
-#             text.set_color("white")
-
-#         # Remove the box around the entire plot
-#         plt.box(False)
-
-#         # Set x and y axis labels to white
-#         plt.xlabel('LAP_NUMBER', color='white')
-#         plt.ylabel('LAP_TIME', color='white')
-
-#         # Set the color of the axis ticks to white
-#         plt.xticks(color='white')
-#         plt.yticks(color='white')
-
-#         # Show the plot
-#         st.pyplot(plt)
-
-#         # Print the slopes and team names
-#         st.write('Teams:', teams)
-#         st.write('Slopes:', slopes)
-
-# # Optionally, you can add more widgets to customize the data analysis further, such as date filters or other user interactions.
-# """END OF WORKING CODE"""
-
-
 import streamlit as st
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
 import os
-
+import pandas as pd
 
 # Streamlit app title
 st.title("Vector Sport Laps Analysis")
@@ -195,20 +63,6 @@ elif page == "Data Analysis":
 
     if csv_file is not None:
             st.write("File uploaded successfully!")
-            import streamlit as st
-            import seaborn as sns
-            # Install Seaborn if not already installed
-            # try:
-            #     import seaborn as sns
-            # except ImportError:
-            #     st.warning("Seaborn not found. Installing...")
-            #     st.system("pip install seaborn")
-            #     st.success("Seaborn has been successfully installed.")
-            # import matplotlib.pyplot as plt
-            # import statsmodels.api as sm  # Import the statsmodels library for lowess smoothing
-            # import os
-            import pandas as pd
-
 
             data_load_state = st.text('Loading data...')
             laps = load_laps_data(10000)
